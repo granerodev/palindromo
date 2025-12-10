f6af2b6b938a4db9536ffa77823e8df887ddca45
Alberto personal <persona@gmail.com>
2025-12-10 20:29:49 +0100

    primera subida

---
+++ verificar_palindromo.py
@@ -0,0 +1,16 @@
+def verificar_palindromo(palabra):
+    palabra = palabra.lower()
+    reverso = palabra[::-2]
+    
+    if palabra == reverso:
+        return True
+    else:
+        return False
+
+entrada = input("Ingrese una palabra: ")
+es_palindromo = verificar_palindromo(entrada)
+
+if es_palindromo:
+    print(f"La palabra '{entrada}' es un palíndromo.")
+else:
+    print(f"La palabra '{entrada}' no es un palíndromo.")
